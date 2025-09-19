8fd5ffbdd1632564f67419fd012d0106a0fd2f09
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-19 16:47:28 -0500

    fix: :bug: Se arreglo la navbar

--- src/components/encabezado/Encabezado.tsx
+++ src/components/encabezado/Encabezado.tsx
@@ -1,13 +1,44 @@
+import { useEffect, useState } from "react";
 import type { JSX } from "react";
 import { headerItems } from "../../data/encabezado/encabezado";
 import type { HeaderItem } from "../../interfaces/encabezado/Encabezado";
 
 const Header = (): JSX.Element => {
+  const [visible, setVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setVisible(false);
+      } else if (window.scrollY < lastScrollY) {
+        setVisible(true);
+      }
+
+      setLastScrollY(window.scrollY);
+
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setVisible(false), 2000);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearTimeout(timeoutId);
+    };
+  }, [lastScrollY]);
+
   return (
     <div className="flex items-center justify-center">
-      <div className="z-50 fixed top-8 flex items-center justify-around space-x-4 p-3 rounded-full bg-gray-300 
-                      w-3xs opacity-50 scale-90 transition-all duration-500 ease-in-out shadow-[0_0_20px_rgba(186,85,211,1)] 
-                      hover:opacity-100 hover:scale-100 hover:bg-gray-100 hover:w-md">
+      <div
+        className={`z-50 fixed top-8 flex items-center justify-around space-x-4 p-3 rounded-full bg-gray-500 
+                    w-3xs opacity-50 scale-90 shadow-[0_0_20px_rgba(186,85,211,1)] 
+                    hover:opacity-100 hover:scale-100 hover:bg-gray-100 hover:w-md
+                    transition-all duration-500 ease-in-out
+                    ${visible ? "translate-y-0 opacity-100" : "-translate-y-40 opacity-0"}`}
+      >
         {headerItems.map(({ icon: Icon }: HeaderItem, index: number) => (
           <Icon
             key={index}
--- src/components/sobreMi/SobreMi.tsx
+++ src/components/sobreMi/SobreMi.tsx
@@ -4,8 +4,8 @@ const SobreMi = () => {
   return (
     <section className="relative w-full h-screen overflow-hidden">
       <ParticlesBackground />
-      <div className="relative z-10 flex justify-center items-center h-full">
-        <h1 className="text-white text-4xl font-bold">Conóceme un poco</h1>
+      <div className="relative z-10 flex justify-center h-full">
+        <h1 className="text-white text-5xl font-bold mt-25 [text-shadow:4px_4px_7px_rgba(207,207,207,0.3)]">Conóceme un poco</h1>
       </div>
     </section>
   );
