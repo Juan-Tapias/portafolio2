f6e2515979ea5c4978809469c231b78024608709
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-19 16:51:52 -0500

    feat:

--- src/components/sobreMi/SobreMi.tsx
+++ src/components/sobreMi/SobreMi.tsx
@@ -5,7 +5,7 @@ const SobreMi = () => {
     <section className="relative w-full h-screen overflow-hidden">
       <ParticlesBackground />
       <div className="relative z-10 flex justify-center h-full">
-        <h1 className="text-white text-5xl font-bold mt-25 [text-shadow:4px_4px_7px_rgba(207,207,207,0.3)]">Conóceme un poco</h1>
+        <h1 className="text-white text-5xl font-bold mt-8 [text-shadow:4px_4px_7px_rgba(207,207,207,0.3)]">Conóceme un poco</h1>
       </div>
     </section>
   );
